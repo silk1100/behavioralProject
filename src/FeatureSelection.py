5bc80cf1c86a32ccf5ca9b0bd4929b4ec5793d4d
kal1000 <mtali003@louisville.edu>
2021-04-30 17:18:01 -0400

    Starting RFECV

---
+++ src/FeatureSelection.py
@@ -0,0 +1,10 @@
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.model_selection import StratifiedKFold
+from sklearn.feature_selection import RFECV
+
+from sklearn import base
+
+class FeatureSelector(base.TransformerMixin, base.BaseEstimator):
+    def __int__(self):
+        pass
+    
